7f9dacb26587b77e5dd5b3d7fcc62a641c979400
Asthrou <asthrou@hotmail.com>
2025-10-06 13:48:30 -0400

    Update app_main.c

--- examples/pc_atx_switch/main/app_main.c
+++ examples/pc_atx_switch/main/app_main.c
@@ -208,6 +208,31 @@ void app_main()
     
     
 
+    switch_device = esp_rmaker_device_create("Switch2", ESP_RMAKER_DEVICE_SWITCH, NULL);
+
+    esp_rmaker_device_add_cb(switch_device, write_cb, NULL);
+
+    /* Add the standard name parameter (type: esp.param.name), which allows setting a persistent,
+     * user friendly custom name from the phone apps. All devices are recommended to have this
+     * parameter.
+     */
+    esp_rmaker_device_add_param(switch_device, esp_rmaker_name_param_create(ESP_RMAKER_DEF_NAME_PARAM, "Reset Switch"));
+
+    /* Add the standard power parameter (type: esp.param.power), which adds a boolean param
+     * with a toggle switch ui-type.
+     */
+    esp_rmaker_param_t *power_param = esp_rmaker_power_param_create(ESP_RMAKER_DEF_POWER_NAME, DEFAULT_POWER);
+    esp_rmaker_device_add_param(switch_device, power_param);
+
+    /* Assign the power parameter as the primary, so that it can be controlled from the
+     * home screen of the phone apps.
+     */
+    esp_rmaker_device_assign_primary_param(switch_device, power_param);
+
+    /* Add this switch device to the node */
+    esp_rmaker_node_add_device(node, switch_device);
+
+
 
     
     
